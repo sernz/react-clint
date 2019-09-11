547842b68a6a310a835fda8c2a40e1be615d6697
vverp <793552120@qq.com>
2019-09-11 17:51:04 +0800

    99

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import {Form,Input,Button,Icon,Checkbox} from 'antd'
+
+import './login.less'
+
+
+function hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+export  default  class  Login extends Component{
+
+    render() {
+        return <div className='login' >
+            <header className='login-header'></header>
+            <div className='login-content'>
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                    <Form.Item>
+                        {getFieldDecorator('username', {
+                            rules: [{ required: true, message: 'Please input your username!' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Username"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: 'Please input your Password!' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(<Checkbox>Remember me</Checkbox>)}
+                        <a className="login-form-forgot" href="">
+                            Forgot password
+                        </a>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                        Or <a href="">register now!</a>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    }
+}
