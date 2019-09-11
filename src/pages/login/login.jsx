e0302c7e4f89c146360b3712a894d5ded4df6079
vverp <793552120@qq.com>
2019-09-11 17:58:38 +0800

    99

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -13,40 +13,29 @@ export  default  class  Login extends Component{
         return <div className='login' >
             <header className='login-header'></header>
             <div className='login-content'>
-                <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form  className="login-form">
                     <Form.Item>
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />,
-                        )}
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="用户名称"
+                        />
                     </Form.Item>
                     <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />,
-                        )}
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="密码"
+                        />
                     </Form.Item>
                     <Form.Item>
-                        {getFieldDecorator('remember', {
-                            valuePropName: 'checked',
-                            initialValue: true,
-                        })(<Checkbox>Remember me</Checkbox>)}
+                        <Checkbox>记住密码</Checkbox>
                         <a className="login-form-forgot" href="">
-                            Forgot password
+                            忘记密码
                         </a>
                         <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
+                            登录
                         </Button>
-                        Or <a href="">register now!</a>
+                        {/*Or <a href="">register now!</a>*/}
                     </Form.Item>
                 </Form>
             </div>
